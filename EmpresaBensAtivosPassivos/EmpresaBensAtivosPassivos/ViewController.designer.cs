bed3e09abb42dc4a580f9746936bfce97df652c9
Núbio Knupp <nubioknupp@gmail.com>
2017-02-16 18:31:06 -0200

    crie layout

--- EmpresaBensAtivosPassivos/EmpresaBensAtivosPassivos/ViewController.designer.cs
+++ EmpresaBensAtivosPassivos/EmpresaBensAtivosPassivos/ViewController.designer.cs
@@ -1,17 +1,66 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace EmpresaBensAtivosPassivos
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UITextField bancoTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField caixaTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField emprestimoTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField fornecedorTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField pagarTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField receberTextField { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (caixaTextField != null) {
+				caixaTextField.Dispose ();
+				caixaTextField = null;
+			}
+
+			if (bancoTextField != null) {
+				bancoTextField.Dispose ();
+				bancoTextField = null;
+			}
+
+			if (receberTextField != null) {
+				receberTextField.Dispose ();
+				receberTextField = null;
+			}
+
+			if (emprestimoTextField != null) {
+				emprestimoTextField.Dispose ();
+				emprestimoTextField = null;
+			}
+
+			if (fornecedorTextField != null) {
+				fornecedorTextField.Dispose ();
+				fornecedorTextField = null;
+			}
+
+			if (pagarTextField != null) {
+				pagarTextField.Dispose ();
+				pagarTextField = null;
+			}
+		}
+	}
 }
