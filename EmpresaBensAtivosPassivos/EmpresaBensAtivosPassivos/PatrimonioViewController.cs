acf764ceda9ad369b0320765fc99d07c01ea265c
Núbio Knupp <nubioknupp@gmail.com>
2017-02-20 20:14:15 -0300

    adicionei um botão calcular e viewcontroller para exibir resultado.

---
+++ EmpresaBensAtivosPassivos/EmpresaBensAtivosPassivos/PatrimonioViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace EmpresaBensAtivosPassivos
+{
+	public partial class PatrimonioViewController : UIViewController
+	{
+		public PatrimonioViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
