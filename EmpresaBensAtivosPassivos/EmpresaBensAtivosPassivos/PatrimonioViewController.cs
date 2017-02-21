709b46b4299eb140eaaa4623bfc6f2afc9274ef2
knupp <knupp@KNUPP-VM>
2017-02-21 20:24:44 -0200

    Adicionei a função para calcular o património da empresa

--- EmpresaBensAtivosPassivos/EmpresaBensAtivosPassivos/PatrimonioViewController.cs
+++ EmpresaBensAtivosPassivos/EmpresaBensAtivosPassivos/PatrimonioViewController.cs
@@ -1,26 +1,29 @@
 ﻿using System;
-
+using System.Globalization;
 using UIKit;
 
 namespace EmpresaBensAtivosPassivos
 {
-	public partial class PatrimonioViewController : UIViewController
-	{
-		public PatrimonioViewController(IntPtr handle) : base(handle)
-		{
-		}
+    public partial class PatrimonioViewController : UIViewController
+    {
+        public double Patrimonio { private get; set; }
 
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-			// Perform any additional setup after loading the view, typically from a nib.
-		}
+        public PatrimonioViewController(IntPtr handle) : base(handle)
+        {
+        }
 
-		public override void DidReceiveMemoryWarning()
-		{
-			base.DidReceiveMemoryWarning();
-			// Release any cached data, images, etc that aren't in use.
-		}
-	}
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            patrimonioTextField.Text = Patrimonio.ToString(CultureInfo.CurrentCulture);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
 }
 
--- EmpresaBensAtivosPassivos/EmpresaBensAtivosPassivos/ViewController.cs
+++ EmpresaBensAtivosPassivos/EmpresaBensAtivosPassivos/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Foundation;
 using UIKit;
 
 namespace EmpresaBensAtivosPassivos
@@ -21,5 +21,37 @@ namespace EmpresaBensAtivosPassivos
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            if (segue.Identifier.Equals("PatrimonioSegueId"))
+            {
+                var patrimonio = segue.DestinationViewController as PatrimonioViewController;
+
+                if (patrimonio != null)
+                {
+                    patrimonio.Patrimonio = CalculaPatrimonio();
+                }
+            }
+
+            base.PrepareForSegue(segue, sender);
+        }
+
+        private double CalculaPatrimonio()
+        {
+            var banco = !string.IsNullOrEmpty(bancoTextField.Text) ? bancoTextField.Text : "0";
+            var caixa = !string.IsNullOrEmpty(caixaTextField.Text) ? caixaTextField.Text : "0";
+            var receber = !string.IsNullOrEmpty(receberTextField.Text) ? receberTextField.Text : "0";
+            var pagar = !string.IsNullOrEmpty(pagarTextField.Text) ? pagarTextField.Text : "0";
+            var fornecedor = !string.IsNullOrEmpty(fornecedorTextField.Text) ? fornecedorTextField.Text : "0";
+            var emprestimo = !string.IsNullOrEmpty(emprestimoTextField.Text) ? emprestimoTextField.Text : "0";
+
+            var ativos = Convert.ToDouble(banco) + Convert.ToDouble(caixa) + Convert.ToDouble(receber);
+            var passivos = Convert.ToDouble(pagar) + Convert.ToDouble(fornecedor) + Convert.ToDouble(emprestimo);
+
+            return ativos - passivos;
+        }
+
+
     }
 }
